eeff5a2cf656202b829e1081b87671db1317aca5
Brendalikukuvali <brendalikukuvali@gmail.com>
2020-09-08 13:17:18 +0300

    commit message

--- src/App.js
+++ src/App.js
@@ -2,231 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function UserGreeting (props) {
-  return<h1>Welcome back!</h1>;
-}
-function GuestGreeting(props) {
-  return<h1>Please sign up.</h1>;
-} 
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return<UserGreeting/>;
-   }
-   return<GuestGreeting/>;
-  }
-  ReactDOM.render(
-    // Try changing to isLoggedIn={true}:
-    <Greeting isLoggedIn={false} />,
-    document.getElementById('root')
-
-  );
-  render() 
-    const isLoggedIn = this.state.isLoggedIn;
-    return (
-    <div>     
-    The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.
-    </div>
-   );
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      type1: "Vegetables",
-
-      veg1: "Spinach",
-      veg1Img:
-       "Images/Spinachzangu.jpg",
-      SpinachImgQuantity: "10kgs",
-      SpinachImgPrice: 100,
 
 
-      veg2: "Onion",
-      veg2Img:
-        "Images/Onionszangu.jpg",
-      OnionsImgQuantity: "30kgs",
-      OnionsImgPrice: 100,
+// class Products extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     products = vegetables.map((veges) => {
+//       [
+//           {
+//       Name:"Spinach",
+//       Img:
+//        "Images/Spinachzangu.jpg",
+//       Quantity: "10kgs",
+//       Price: 100,
+      
+//       Name: "Onion",
+//       Img:
+//         "Images/Onionszangu.jpg",
+//       Quantity: "30kgs",
+//       Price: 100,
+//       }
+//     ]
+        
+//     ),  
+//       render() {
+//         return (
+//           <div>
+//             <h1>GREENS KIOSK</h1>
+//             <h2> Welcome To Our New Site </h2>
+//             <section className="products">
+//               <div className="product-card">
+//                 <div className="product-image">
+//                   <img className="photo" src={veges.Img} alt="" />
+//                 </div>
+//                 <div className="product-info">
+//                   <h5>Name </h5>
+//                   <p> Kshs. {veges.Price}</p>
+//                   <p> Quantity {veges.Quantity} </p>
+//                   <button className="add-to-cart-btn" onClick="saveItem()">
+//                     Add To cart
+//                   </button>
+//                 </div>
+//               </div>
+//               </section>
+//       </div>
+//     );
+//   }
+// }
 
-
-      veg3: "Carrots",
-      veg3Img:
-        "Images/Carrotszangu.jpz",
-      CarrotsImgQuantity: "50kgs",
-      CarrotsImgPrice: 100,
-
-
-      type2: "Fruits",
-
-      fruit1: " Banana",
-      fruit1Img:
-        "Images/Bananaszangu.jpg",
-      bananasImgQuantity: "20kgs",
-      BananasImgPrice: 100,
-
-
-      fruit2: " Mango",
-      fruit2Img:
-        "Images/Mangoeszangu.jpg",
-      MangoesImgQuantity: 50,
-      MangoesImgPrice: 100,
-
-
-      appleImg:
-        "Images/Appleszangu.png",
-      ApplesImgQuantity: 30,
-      ApplesImgPrice: 100,
-
-      bunchImg:
-        "Images/Orangeszangu.jpg",
-        ApplesImgQuantity: 30,
-      ApplesImgPrice: 100,
-
-      bunch2Img:
-        "Images/Tomatoeszangu.jpg",
-      ApplesImgQuantity: 30,
-      ApplesImgPrice: 100,
-
-      bunch3Img:
-        "Images/Berrieszangu.jpg",
-      ApplesImgQuantity: 30,
-      ApplesImgPrice: 100,
-    };
-  }
-
-  render() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>GREENS KIOSK</h1>
-        <h2> Welcome to our new site </h2>
-        <header>
-<nav>
-<ul>
-<li><a href="index.html" class="active">Vegetables</a></li>
-<li><a href="about.html">Fruits</a></li>
-</ul>
-</nav>
-</header>
-
-        <section className="products">
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.appleImg} alt="" />
-            </div>
-            <div className="product-info">
-              <h5>Apples </h5>
-              <p> Kshs. {this.state.appleImgPrice}</p>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.veg1Img} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Spinach</h5>
-              <h6> Kshs. </h6>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.veg2Img} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Onions</h5>
-              <h6> Kshs.</h6>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.bunchImg} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Oranges</h5>
-              <p> Kshs. {this.state.appleImgPrice}</p>
-              <p> Quantity {this.state.appleImgQuantity} </p>
-          <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.appleImg} alt="" />
-            </div>
-            <div className="product-info">
-              <h5>Spinach </h5>
-              <p> Kshs. {this.state.appleImgPrice}</p>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.veg1Img} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Onions</h5>
-              <h6> Kshs. </h6>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.veg2Img} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Carrots</h5>
-              <h6> Kshs.</h6>
-              <p> Quantity {this.state.spinachImgQuantity} </p>
-              <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          <div className="product-card">
-            <div className="product-image">
-              <img className="photo" src={this.state.bunchImg} alt="" />
-            </div>
-            <div className="product-info">
-              <h5> Banana</h5>
-              <p> Kshs. {this.state.appleImgPrice}</p>
-              <p> Quantity {this.state.appleImgQuantity} </p>
-          <button className="add-to-cart-btn" onClick="saveItem()">
-                Add To cart
-              </button>
-            </div>
-          </div>
-
-          
-           </section>
-      </div>
-    );
-  }
-}
-
-export default App;
-
+//export default Products;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,200 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default class Products extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      type:["Fruits","Fruits","Fruits","Fruits","Fruits","Fruits","Fruits","Fruits","Vegetables","Vegetables","Vegetables","Vegetables","Vegetables","Vegetables","Vegetables","Vegetables"],
+      name:["Apples","Grapes","Berries","Mangoes","Oranges","Bananas","Avocados","Pawpaw","Tomatoes","Onionss","Beet roots","Carrots","Cucumber","Brocollis","Kales","Spinach"],
+      quantity:[15,20,10,30,40,30,35,45,50,55,70.65,70,80,75,80],
+      price:[350,300,250,150,360,200,130,125,245,180,165,180,135,190,235,260]
+    };
+  }
+  render(){
+    return(
+      <div className="container">
+       <h1 className="cont"> Grocery Store</h1>
+       <h1 className="conta"> Vegetables and Fruits </h1>
+       <div className="flex">
+        <div className ="photo">
+        <img src='./Images/Groceryzangu.jpg' />
+        </div>
+         <div>
+           <img src='./Images/Appleszangu.png' width="300" height="300"/>
+           <p className="para">
+           <p>Name: {this.state.name[0]}</p>
+           <p>Type:{this.state.type[0]}</p>
+           <p>Quantity: {this.state.quantity[3]}</p>
+          <p>Price: {this.state.price[0]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Grapeszangu.jpg' width="300" height="300"/>
+           <p className="para">
+           <p>Name:{this.state.name[1]}</p> 
+           <p>Type:{this.state.type[1]}</p> 
+           <p>Quantity:{this.state.quantity[1]}</p>
+           <p>Price: {this.state.price[1]}</p>
+           <p className="button">Add To Cart</p>
+           </p>
+        </div>
+        <div>  
+          <img src='./Images/Berieszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[2]}</p>
+          <p>Type:{this.state.type[2]}</p>
+          <p>Quantity:{this.state.quantity[2]}</p>   
+          <p>Price: {this.state.price[2]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/MangosZangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[3]}</p>
+          <p>Type:{this.state.type[3]}</p>
+          <p>Quantity:{this.state.quantity[3]}</p>   
+          <p>Price: {this.state.price[3]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Orangeszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[4]}</p>
+          <p>Type:{this.state.type[4]}</p>
+          <p>Quantity:{this.state.quantity[4]}</p>   
+          <p>Price: {this.state.price[4]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Avocadozangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[5]}</p>
+          <p>Type:{this.state.type[5]}</p>
+          <p>Quantity:{this.state.quantity[5]}</p>   
+          <p>Price: {this.state.price[5]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Bananaszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[6]}</p>
+          <p>Type:{this.state.type[6]}</p>
+          <p>Quantity:{this.state.quantity[6]}</p>   
+          <p>Price: {this.state.price[6]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Pawpawzangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[7]}</p>
+          <p>Type:{this.state.type[7]}</p>
+          <p>Quantity:{this.state.quantity[7]}</p>   
+          <p>Price: {this.state.price[7]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Tomatoeszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[8]}</p>
+          <p>Type:{this.state.type[8]}</p>
+          <p>Quantity:{this.state.quantity[8]}</p>   
+          <p>Price: {this.state.price[8]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Onionszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[9]}</p>
+          <p>Type:{this.state.type[9]}</p>
+          <p>Quantity:{this.state.quantity[9]}</p>   
+          <p>Price: {this.state.price[9]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Beetrootzangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[10]}</p>
+          <p>Type:{this.state.type[10]}</p>
+          <p>Quantity:{this.state.quantity[10]}</p>   
+          <p>Price: {this.state.price[10]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>     
+        <div>
+          <img src='./Images/Carrotszangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[11]}</p>
+          <p>Type:{this.state.type[11]}</p>
+          <p>Quantity:{this.state.quantity[11]}</p>   
+          <p>Price: {this.state.price[11]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Cucuzzangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[12]}</p>
+          <p>Type:{this.state.type[12]}</p>
+          <p>Quantity:{this.state.quantity[12]}</p>   
+          <p>Price: {this.state.price[12]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Brocollirootzangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[13]}</p>
+          <p>Type:{this.state.type[13]}</p>
+          <p>Quantity:{this.state.quantity[13]}</p>   
+          <p>Price: {this.state.price[13]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Kalezangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[14]}</p>
+          <p>Type:{this.state.type[14]}</p>
+          <p>Quantity:{this.state.quantity[14]}</p>   
+          <p>Price: {this.state.price[14]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+        <div>
+          <img src='./Images/Spinachzangu.jpg' width="300" height="300"/>
+          <p className="para">
+          <p>Name:{this.state.name[15]}</p>
+          <p>Type:{this.state.type[15]}</p>
+          <p>Quantity:{this.state.quantity[15]}</p>   
+          <p>Price: {this.state.price[15]}</p>
+          <p className="button">Add To Cart</p>
+          </p>
+        </div>
+       </div>
+       </div>
+    );
+    } 
+
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Products />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+
+
